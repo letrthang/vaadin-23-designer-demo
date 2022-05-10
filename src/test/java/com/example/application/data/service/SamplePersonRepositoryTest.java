4cd3bf8f82d7868bb354dcf5eaeb5d39b5c3e5e9
LeTrThang <letrthang@gmail.com>
2022-05-10 19:23:33 +0800

    add test dir

---
+++ src/test/java/com/example/application/data/service/SamplePersonRepositoryTest.java
@@ -0,0 +1,17 @@
+package com.example.application.data.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SamplePersonRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
